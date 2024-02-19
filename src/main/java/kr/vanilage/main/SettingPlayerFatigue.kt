43341c6e0d0c1f5ec7599ab85a2689e36eaf0f8f
MAKEREKAM <kimyounghyun123456@gmail.com>
2024-02-19 16:59:57 +0900

    Update

--- src/main/java/kr/vanilage/main/SettingPlayerFatigue.kt
+++ src/main/java/kr/vanilage/main/SettingPlayerFatigue.kt
@@ -44,7 +44,7 @@ class SettingPlayerFatigue : Listener {
                     Main.playerFatigue[player.uniqueId] = point
 
                     bossBar.progress = point
-                    bossBar.setTitle(String.format("§b피로도 §e%.1f%", Main.playerFatigue[player.uniqueId]!! * 100))
+                    bossBar.setTitle(String.format("§b피로도 §e%.1f%%", Main.playerFatigue[player.uniqueId]!! * 100))
 
                     if (player.isOnline) Bukkit.getScheduler().runTaskLater(Main.instance, this, 1)
                 }
